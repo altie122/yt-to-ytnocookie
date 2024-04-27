c2c091508f3f6f361b01c3b9bfa187b163ebe626
altie122 <77561831+altie122@users.noreply.github.com>
2024-04-27 02:06:19 +0000

    add "-" and "_" to the youtubeVideoIdCharacters array

--- app/lib/yt.ts
+++ app/lib/yt.ts
@@ -5,7 +5,7 @@ const youtubeVideoIdCharacters: string[] = [
   'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
   'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
   'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-  'y', 'z'
+  'y', 'z', '-', '_'
 ];
 
 
