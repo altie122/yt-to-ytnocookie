ea33c0276a796c56aeedf19e2df796ee09f08fc1
altie122 <77561831+altie122@users.noreply.github.com>
2024-06-25 18:56:10 -0600

    final beta push

--- app/routes/video.$id.tsx
+++ app/routes/video.$id.tsx
@@ -1,27 +1,42 @@
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { useParams } from "@remix-run/react";
+import { json, useParams } from "@remix-run/react";
 import { SendDiscordWebhook } from "~/lib/webhook.server";
 import { blockedUserAgents } from "~/lib/consts";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "YT To YT No Cookie" },
-    { name: "description", content: "Simple little website that takes a YouTube URL (Share and Embed URL's and Video ID's are also supported) and turns it into a YouTube nocookie URL" },
-  ];
-};
-
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const userAgent = request.headers.get('User-Agent') || 'Unknown';
-  const id = params.id; // Retrieve the id parameter correctly
+  const { id } = params; // Retrieve the id parameter correctly
+  const requestUrl = new URL(request.url);
+  const siteUrl = requestUrl.protocol + "//" + requestUrl.host;
   console.log(userAgent)
   if (!blockedUserAgents.includes(userAgent)) {
-    const formattedMsg = `Converted Video:\nhttps://yt-to-ytnocookie.dovahkiin.xyz/video/${id}\n\nUserAgent:\n${userAgent}\n\nVideo Converted:\nhttps://www.youtube.com/watch?v=${id}\n\nOutput:\nhttps://www.youtube-nocookie.com/embed/${id}`;
+    const formattedMsg = `Converted Video:\n${siteUrl}/video/${id}\n\nUserAgent:\n${userAgent}\n\nVideo Converted:\nhttps://www.youtube.com/watch?v=${id}\n\nOutput:\nhttps://www.youtube-nocookie.com/embed/${id}`;
     SendDiscordWebhook(formattedMsg);
     console.log(userAgent)
   }
-  return null;
+  return json(
+    { siteUrl }
+  );
 }
+export const meta: MetaFunction<typeof loader> = (args) => {
+  const { data, params } = args;
+  const { id } = params;
+  return [
+    { title: "YT To YT No Cookie" },
+    { name: "description", content: "Simple little website that takes a YouTube URL (Share and Embed URL's and Video ID's are also supported) and turns it into a YouTube nocookie URL" },
+    { property: "og:url", content: `${data?.siteUrl}/video/${id}` },
+    { name: "og:title", content: "YT To YT No Cookie" },
+    { name: "og:description", content: "Simple little website that takes a YouTube URL (Share and Embed URL's and Video ID's are also supported) and turns it into a YouTube nocookie URL" },
+    { property: "og:image", content: `${data?.siteUrl}/img/${id}?id=${id}` },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:creator", content: "@altie122" },
+    { name: "twitter:site", content: "@altie122" },
+    { name: "twitter:title", content: "YT To YT No Cookie" },
+    { name: "twitter:description", content: "Simple little website that takes a YouTube URL (Share and Embed URL's and Video ID's are also supported) and turns it into a YouTube nocookie URL" },
+    { name: "twitter:image", content: `${data?.siteUrl}/img/${id}?id=${id}` },
 
+  ];
+};
 
 export default function Index() {
   const {id} = useParams();
---
+++ app/ui/api-accordion.tsx
@@ -0,0 +1,79 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "~/components/ui/accordion"
+
+import { Separator } from "~/components/ui/separator";
+
+export default function ApiAccordion() {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Full</AccordionTrigger>
+        <AccordionContent>
+        Description: Returns the Response of all API paths
+        <Separator />
+        Path: https://yt-to-ytnocookie.dovahkiin.xyz/api/full
+        <br />
+        Usage: https://yt-to-ytnocookie.dovahkiin.xyz/api/full?url={'{Youtube URL}'}
+        <br />
+        Response:{'{"IsYT":true,"URL":"{Youtube Nocookie URL}","ID":"{Youtube ID}"}'} OR a 400 error will be served
+        <Separator />
+        Example: https://yt-to-ytnocookie.dovahkiin.xyz/api/full?url=https://www.youtube.com/watch?v=87TdQAGmeOA
+        <br />
+        Example output: {'{"IsYT":true,"URL":"https://www.youtube-nocookie.com/embed/87TdQAGmeOA","ID":"87TdQAGmeOA"}'}
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is Youtube?</AccordionTrigger>
+        <AccordionContent>
+        Description: Returns if the URL is a Youtube URL as a boolen
+        <Separator />
+        Path: https://yt-to-ytnocookie.dovahkiin.xyz/api/isyt
+        <br />
+        Usage: https://yt-to-ytnocookie.dovahkiin.xyz/api/isyt?url={'{Youtube URL}'}
+        <br />
+        Response:{'{"IsYT":{true/false}}'}
+        <Separator />
+        Example: https://yt-to-ytnocookie.dovahkiin.xyz/api/isyt?url=https://www.youtube.com/watch?v=87TdQAGmeOA
+        <br />
+        Example output: {'{"IsYT":true}'}
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Youtube ID</AccordionTrigger>
+        <AccordionContent>
+        Description: Returns the ID of the given Youtube URL
+        <Separator />
+        Path: https://yt-to-ytnocookie.dovahkiin.xyz/api/ytid
+        <br />
+        Usage: https://yt-to-ytnocookie.dovahkiin.xyz/api/ytid?url={'{Youtube URL}'}
+        <br />
+        Response:{'{"ID":"{Youtube ID}"}'} OR a 400 error will be served
+        <Separator />
+        Example: https://yt-to-ytnocookie.dovahkiin.xyz/api/ytid?url=https://www.youtube.com/watch?v=87TdQAGmeOA
+        <br />
+        Example output: {'{"ID":"{87TdQAGmeOA}"}'}
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger>YT to YT No Cookie</AccordionTrigger>
+        <AccordionContent>
+        Description: Returns the youtube-nocookie.com version of the given Youtube URL/ID
+        <Separator />
+        Path: https://yt-to-ytnocookie.dovahkiin.xyz/api/yttoytnocookie
+        <br />
+        Usage: https://yt-to-ytnocookie.dovahkiin.xyz/api/yttoytnocookie?url={'{Youtube URL}'}
+        <br />
+        Response:{'{"URL":"{Youtube Nocookie URL}"}'} OR a 400 error will be served
+        <Separator />
+        Example: https://yt-to-ytnocookie.dovahkiin.xyz/api/yttoytnocookie?url=https://www.youtube.com/watch?v=87TdQAGmeOA
+        <br />
+        Example output: {'{"URL":"https://www.youtube-nocookie.com/embed/87TdQAGmeOA"}'}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
