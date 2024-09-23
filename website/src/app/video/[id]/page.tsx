3000e3b2979637be31a4f24ef2ae0557f884d020
Altie122 <77561831+altie122@users.noreply.github.com>
2024-09-23 12:02:27 -0600

    make video/[id] page

--- website/src/app/video/[id]/page.tsx
+++ website/src/app/video/[id]/page.tsx
@@ -1,8 +1,18 @@
 export default function VideoPage({ params }: { params: { id: string } }) {
-  const id = params.id
+  const id = params.id;
+  const URL = "https://www.youtube-nocookie.com/embed/" + id;
   return (
-    <main>
-      Video: {id}
-    </main>
+    <div className="container mx-auto">
+      <h2 className="text-lg no-underline hover:underline md:text-2xl">
+        {URL}
+      </h2>
+      <h1 className="text-lg">Embed:</h1>
+      <iframe
+        className="mx-auto aspect-video w-full rounded-2xl"
+        title="example"
+        src={URL}
+        allowFullScreen
+      ></iframe>
+    </div>
   );
 }
