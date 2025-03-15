dcf33d8587311123e8c880925936f3919ddb26d7
altie122 <altielynn@gmail.com>
2025-03-14 22:05:17 -0600

    add og image to home page

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,4 +1,58 @@
 import { Convert_bar } from "~/components/client/convert-page";
+import { headers } from "next/headers";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const headersList = await headers();
+  const protocol =
+    headersList.get("protocol") ??
+    headersList.get("x-forwarded-proto") ??
+    "https";
+  const protocolFormatted = protocol.endsWith("://")
+    ? protocol
+    : protocol + "://";
+  const siteUrl = protocolFormatted + headersList.get("host");
+  const id = (await params).id;
+  return {
+    openGraph: {
+      title: "YT To YT No Cookie",
+      description: `Simple little website that takes a YouTube URL (Share and Embed URL's and Video ID's are also supported) and turns it into a YouTube nocookie URL`,
+      url: `${siteUrl}/`,
+      siteName: "YT To YT No Cookie",
+      locale: "en_US",
+      type: "website",
+      images: [
+        {
+          url: `${siteUrl}/social-background.png`,
+          width: 2400,
+          height: 1256,
+          alt: "YT To YT No Cookie",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "YT To YT No Cookie",
+      description:
+        "Simple little website that takes a YouTube URL (Share and Embed URL's and Video ID's are also supported) and turns it into a YouTube nocookie URL",
+      siteId: "866355501432438784",
+      site: "@altie122",
+      creator: "@altie122",
+      creatorId: "866355501432438784",
+      images: [
+        {
+          url: `${siteUrl}/social-background.png`,
+          width: 2400,
+          height: 1256,
+          alt: "YT To YT No Cookie",
+        },
+      ],
+    },
+  };
+}
 
 export default function HomePage() {
   return (
